d5a08b561e1b7952c15b90d007dfc16122922396
chickin_ma <chichkin_ma@elprib.ru>
2017-08-08 10:45:12 +0300

    Изменена структура в соответствии с требованиями.

---
+++ index.js
@@ -0,0 +1,7 @@
+function validate() {
+    var fio = document.getElementById("fioInput").value;
+    var email = document.getElementById("emailInput").value;
+    var phone = document.getElementById("phoneInput").value;
+
+    alert (fio + " " + email + " " + phone);
+}
