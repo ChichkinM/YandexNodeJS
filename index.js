925cf9f116d616cf2a31d1759bdbf9ee76c6e692
chickin_ma <chichkin_ma@elprib.ru>
2017-08-11 13:10:46 +0300

    Релизная версия.
    Удалены bootstrap темы.
    Добавлена еще одна проверка для фио (нельзя на 2-х языках писать одновременно).
    Убраны логи.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var fioFormat = /[a-zа-я \-']+$/i;
 var fioErrorUppercase = /[a-zа-я][A-ZА-Я]+|[A-ZА-Я]{2,}| [a-zа-я]+|^[a-zа-я]/;
+var fioLanguageRu = /[а-я]+/i;
+var fioLanguageEn = /[a-z]+/i;
 var emailFormat = /[A-Za-z0-9\-\.]+@(ya.ru|yandex.ru|yandex.ua|yandex.by|yandex.kz|yandex.com)$/;
 var emailErrorRepSym = /(\.){2,}|(\-){2,}/;
 var emailErrorLateralSym = /(^[\.\-[0-9])|([\.\-]@)/;
@@ -10,7 +12,7 @@ var phoneSymForDel = /[^[0-9]/g;
 var phoneSumLimit = 30;
 var json = ["resources/json/success.json", "resources/json/progress.json", "resources/json/error.json"];
 
-var ui = {
+var uiId = {
     inpFio: "fioInput",
     inpEmail: "emailInput",
     inpPhone: "phoneInput",
@@ -39,34 +41,30 @@ function validate(data) {
     var isValid = true;
     var error = new Array();
 
-    console.log(ui.test1);
-
-    if (data.fio.split(' ').length != 3 || data.fio.match(fioFormat) == null || data.fio.match(fioErrorUppercase) != null) {
+    if (data.fio.split(' ').length != 3 ||
+        data.fio.match(fioFormat) == null ||
+        data.fio.match(fioErrorUppercase) != null ||
+        (data.fio.match(fioLanguageRu) != null && data.fio.match(fioLanguageEn) != null)) {
         isValid = false;
-        error[error.length] = ui.inpFio;
+        error[error.length] = uiId.inpFio;
     }
 
-    if (data.email.match(emailFormat) == null || data.email.match(emailErrorRepSym) != null || data.email.match(emailErrorLateralSym) != null) {
+    if (data.email.match(emailFormat) == null ||
+        data.email.match(emailErrorRepSym) != null ||
+        data.email.match(emailErrorLateralSym) != null) {
         isValid = false;
-        error[error.length] = ui.inpEmail;
+        error[error.length] = uiId.inpEmail;
     }
 
     var phoneSum = 0;
     var phoneStr= data.phone.replace(phoneSymForDel, '');
-    console.log(phoneStr);
     for(var i = phoneStr.length - 1; i >= 0; i--)
         phoneSum += parseInt(phoneStr[i]);
 
     if (data.phone.match(phoneFormat) == null || phoneSum > phoneSumLimit) {
         isValid = false;
-        error[error.length] = ui.inpPhone;
+        error[error.length] = uiId.inpPhone;
     }
-
-
-    console.log(data.fio.match(fioFormat) + " + " + data.fio.match(fioErrorUppercase) + " | " + data.fio);
-    console.log(data.email.match(emailFormat) + " " + data.email.match(emailErrorRepSym) + " " + data.email.match(emailErrorLateralSym) + " " + data.email.match(emailErrorSize));
-    console.log(data.phone.match(phoneFormat) + " " + phoneSum);
-
 
     return {
         isValid: isValid,
@@ -76,29 +74,29 @@ function validate(data) {
 
 function getData() {
     var data = new Object();
-    data.fio = document.getElementById(ui.inpFio).value.trim();
-    data.email = document.getElementById(ui.inpEmail).value.trim();
-    data.phone = document.getElementById(ui.inpPhone).value.trim();
+    data.fio = document.getElementById(uiId.inpFio).value.trim();
+    data.email = document.getElementById(uiId.inpEmail).value.trim();
+    data.phone = document.getElementById(uiId.inpPhone).value.trim();
 
     return data;
 }
 
 function setData(data) {
-    document.getElementById(ui.inpFio).value = data.fio;
-    document.getElementById(ui.inpEmail).value = data.email;
-    document.getElementById(ui.inpPhone).value = data.phone;
+    document.getElementById(uiId.inpFio).value = data.fio;
+    document.getElementById(uiId.inpEmail).value = data.email;
+    document.getElementById(uiId.inpPhone).value = data.phone;
 }
 
 function submit() {
     var data = getData();
     var validateResult = validate(data);
 
-    if (validateResult.errorFields.indexOf(ui.inpFio) == -1)
-        document.getElementById(ui.inpFio).classList.remove(uiProperty.classError);
-    if (validateResult.errorFields.indexOf(ui.inpEmail) == -1)
-        document.getElementById(ui.inpEmail).classList.remove(uiProperty.classError);
-    if (validateResult.errorFields.indexOf(ui.inpPhone) == -1)
-        document.getElementById(ui.inpPhone).classList.remove(uiProperty.classError);
+    if (validateResult.errorFields.indexOf(uiId.inpFioId) == -1)
+        document.getElementById(uiId.inpFio).classList.remove(uiProperty.classError);
+    if (validateResult.errorFields.indexOf(uiId.inpEmail) == -1)
+        document.getElementById(uiId.inpEmail).classList.remove(uiProperty.classError);
+    if (validateResult.errorFields.indexOf(uiId.inpPhone) == -1)
+        document.getElementById(uiId.inpPhone).classList.remove(uiProperty.classError);
 
     if (!validateResult.isValid)
         validateResult.errorFields.forEach(function (field, errorFields) {
@@ -107,7 +105,7 @@ function submit() {
                 element.className += " " + uiProperty.classError;
         });
     else {
-        // document.getElementById(ui.btnSubmit).setAttribute("disabled", "disabled");
+        document.getElementById(uiId.btnSubmit).setAttribute("disabled", "disabled");
         sendRequest();
 
         function sendRequest() {
@@ -118,7 +116,7 @@ function submit() {
             if (request.status == 200) {
 
                 var jsonStatus = JSON.parse(request.responseText)[jsonFields.fieldStatus];
-                var resultContainer = document.getElementById(ui.divResult);
+                var resultContainer = document.getElementById(uiId.divResult);
 
                 if (jsonStatus != jsonFields.statusProgress) {
                     switch (jsonStatus) {
